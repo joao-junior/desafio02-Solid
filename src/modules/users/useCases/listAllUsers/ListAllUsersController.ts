25f0b3990f027887afac9ecc8d30fd53d00e0874
Joao Junior <joaojunior346@gmail.com>
2021-04-01 10:15:53 -0300

    criaçao de rotas desafio solid

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,12 +1,24 @@
 import { Request, Response } from "express";
+import { IncomingHttpHeaders } from "http";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
+interface IHeaders extends IncomingHttpHeaders {
+  user_id: string;
+}
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = <IHeaders>request.headers;
+
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.json(users);
+    } catch (err) {
+      return response.status(400).json({ error: "Mensagem do erro" });
+    }
   }
 }
 
